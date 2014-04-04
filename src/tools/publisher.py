7cc2df389aad11b6f091383a6b93992cadfe7da1
Adam Damiano <onaimada@gmail.com>
2014-04-04 16:31:11 -0700

    * The minified JS file now contains all licenses used.

--- src/tools/publisher.py
+++ src/tools/publisher.py
@@ -51,6 +51,62 @@ uglifyJsPath = None
 # this script, that way all of the scripts/css files will still resolve.
 pathFromOutputToInput = ''
 
+soundManager2License = """
+SoundManager 2 License:
+SoundManager 2: JavaScript Sound for the Web
+----------------------------------------------
+http://schillmania.com/projects/soundmanager2/
+
+Copyright (c) 2007, Scott Schiller. All rights reserved.
+Code provided under the BSD License:
+http://schillmania.com/projects/soundmanager2/license.txt
+
+V2.97a.20130512
+"""
+jQueryAndJQueryUILicense = """
+JQuery and JQueryUI license: https://jquery.org/license/
+"""
+jGesturesLicense = """
+jGestures license:
+Copyright 2010-2011 Neue Digitale / Razorfish GmbH
+Copyright 2011-2012, Razorfish GmbH
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+modernizrLicense = """
+modernizr license:
+Copyright (c) 2009-2013
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
+hammerLicense = """
+Hammer.js license:
+Hammer.JS - v1.0.6dev - 2013-07-12
+http://eightmedia.github.com/hammer.js
+Copyright (c) 2013 Jorik Tangelder <j.tangelder@gmail.com>;
+Licensed under the MIT license 
+"""
+
 # Concatenates the contents of each file.
 # jsFiles - an array of paths (they can be relative) to each JavaScript file.
 def concatenateAllFiles(jsFiles) :
@@ -181,6 +237,13 @@ def minifyFile() :
     output = subprocess.check_output(['node', uglifyJsPath, minifiedJsFilePath])
 
     with open(minifiedJsFilePath, 'w') as minifiedJsFile :
+        minifiedJsFile.write('/**')
+        minifiedJsFile.write(soundManager2License)
+        minifiedJsFile.write(jQueryAndJQueryUILicense)
+        minifiedJsFile.write(jGesturesLicense)
+        minifiedJsFile.write(modernizrLicense)
+        minifiedJsFile.write(hammerLicense)
+        minifiedJsFile.write('*/')
         minifiedJsFile.write(output)
 
 def main() :
@@ -195,7 +258,7 @@ def main() :
     print "Done. Paths:"
     print "\tMinified JavaScript file: %s" % (minifiedJsFilePath)
     print "\tModified index.html: %s" % (outputHtmlFilePath)
-    print "\Modified CSS files: this directory (*.min.css)"
+    print "\tModified CSS files: this directory (*.min.css)"
 
 if __name__ == '__main__':
     main()
